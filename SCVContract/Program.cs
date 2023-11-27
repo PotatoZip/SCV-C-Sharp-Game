19d1b167667cbd54e90381d511bdd98079bc6456
WiktorZmiendak <wiktor.zmiendak@gmail.com>
2023-11-27 16:28:25 +0100

    Send SCV project to remote repository

---
+++ SCVContract/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SCVContract {
+    internal class Program {
+        static void Main(string[] args) {
+            Menu menu = new Menu();
+            menu.StartGame(menu);
+        }
+    }
+}
